b52eba7fab2033565ca651b6bf08e9aed4ecc4b1
Unknown <shaui940123@gmail.com>
2020-08-12 22:47:35 +0800

    modify the CoursePage
    1. CoursePage現在會正確呈現使用者在Forum點選的課程了(但若課程資料有缺則會error)
    2. CoursePage可以讀取html格式的課程簡介(intro)了
    3. 可點亮標準的width小小調整

--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -3,6 +3,7 @@ import './CoursePage.css';
 import $ from 'jquery';
 import FirebaseMg from '../Utils/FirebaseMg.js';
 import UserContext from '../Contexts/UserContext'
+import parse from 'html-react-parser';
 
 const fbMg = new FirebaseMg()
 var root = fbMg.myRef
@@ -35,17 +36,18 @@ function Course(props) {
 		</div>
       <div className="CoursePage row course-icon">
       	<div className="col">
-      		<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRw728JESaDNMSj_h3zrUUD49Xwgf0WNGAQcZKn7Kk9aZm2fGMK&usqp=CAU" className="img-thumbnail float-left" alt="Cinque Terre" style={{'width':'20vw'}}/>
+      		<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRw728JESaDNMSj_h3zrUUD49Xwgf0WNGAQcZKn7Kk9aZm2fGMK&usqp=CAU" className="img-thumbnail float-left" alt="Cinque Terre" style={{'width':'20vw','marginBottom':'10px'}}/>
       		{/* <img src={imgURL} className="img-thumbnail float-left" alt="Cinque Terre" style={{'width':'20vw','marginBottom':'10px'}}/> */}
-		<div className="CoursePage row course-introduction" style={{}}>
+		<div className="CoursePage row course-introduction" style={{'height':'35%'}}>
 			<div className="col">
-		    	<p className="CoursePage text-justify" style={{'fontSize':'16pt'}}>{intro}</p>
+
+				<p className="CoursePage text-justify" style={{'fontSize':'16pt'}}>{parse(intro)}</p>
 			</div>
       	</div>
-	  <div className="CoursePage row" style={{}}>
+	  <div className="CoursePage row" style={{'height':'45%'}}>
 	  	<GetSkills node={node} level={level} />
 	  </div>
-      <div className="CoursePage row course-link" style={{'marginTop':'7px'}}>
+      <div className="CoursePage row course-link" style={{'height':'20%'}}>
 		<div className="col">
 			<GetLinks links={links} />
 			<button className='CoursePage btn btn-info' style={{'float':'right'}} onClick={updateUserState}>完成課程</button>
@@ -109,9 +111,9 @@ function Comment(props) {
 				</div>
 				<div className="CoursePage content">
 					<span class="CoursePage user-name">Louis</span>
-					<span style={{'marginLeft':'10px'}}> 2020/8/7</span>
+					<span style={{'marginLeft':'10px'}}> 2020/9/9</span>
 					<p className="CoursePage card-text">
-						明明說好了3個罐罐，最後卻只有1個，像極了愛情♀♂
+						關注了Tensorflow For CNN Cifar-10
 					</p>
 				</div>
 				<div className="CoursePage actions">
@@ -131,9 +133,12 @@ class CoursePage extends Component{
 	constructor(props){
 		super(props)
 		this.state = {
-			id:"3ee4e691-18de-4060-a466-dda24cf94b22",
-			name:"JAVA 基礎教學",
-			level:"JAVA 1級",
+			// id:"3ee4e691-18de-4060-a466-dda24cf94b22",
+			// name:"JAVA 基礎教學",
+			// level:"JAVA 1級",
+			id:props.location.state.id,
+			name:props.location.state.name,
+			level:props.location.state.level,
 			intro:"",
 			node:[],
 			links:[],
@@ -144,6 +149,8 @@ class CoursePage extends Component{
 	}
 
 	componentDidMount(){
+		console.log('id=',this.state.id)
+		console.log('level=',this.state.level)
 
 		var myRef=root.child('Posts/'+this.state.level+'/'+this.state.id+'/course')
 		myRef.once('value',(snapshot)=>{
@@ -168,7 +175,6 @@ class CoursePage extends Component{
 			let path1 = 666 //儲存Skill的Index
 			let path2 = [] //儲存Std的Index
 			let stdName = []
-			// let stdName = new Object() //紀錄應點亮的節點名稱，用來對應path
 			let childrenLength = 666 //計算Progress用的參數
 
 			//儲存各階層的index
@@ -180,21 +186,13 @@ class CoursePage extends Component{
 
 					skill["children"].forEach( (std, index) =>{
 						if(skillStd.includes(std["name"])){
-							path2.push(index) 
+							path2.push(index)
 							stdName.push(std["name"])
-							// stdName[index] = std["name"] 
 						}
 					})
 				}
 			})
 
-			//更新progress中的進度
-			this.updateProgress(skillName, stdName, childrenLength)
-
-			//更新treeState中的進度
-			let treeStatePath = path + "/" + path1
-			this.updateTreeStateProgress(treeStatePath, stdName.length, childrenLength)
-			
 			//遍歷每個Std，點亮未點亮的部分
 			path2.forEach((pathIndex, index)=>{
 				let skillStdPath = path + "/" + path1 + "/children/" + pathIndex + "/nodeSvgShape/shapeProps"
@@ -202,24 +200,22 @@ class CoursePage extends Component{
 
 				skillStdRef.once('value', (snapshot) => {
 					let skillStdData = snapshot.val()
-					
 					console.log("skillStdData:", skillStdData)
 
 					if(skillStdData["fill"] !== "yellow"){
 						skillStdRef.update({"fill": "yellow"}) //點亮節點
-						// newProgreeNode.push(stdName[index])
 						console.log("skillStdData:", skillName, stdName[index], childrenLength)
-
+						this.updateProgress(skillName, stdName[index], childrenLength)
 					}
-				}).then(() =>{
-
 				})
 			})
 
+
     	}).then( (result) => {
-			alert("恭喜習得新技能!")
+			alert("操作成功")
     	}).catch( (failureCallback) =>{
 			// console.log("failureCallback:",failureCallback)
+			alert("操作失敗，請再試一次")
     	});
 	}
 
@@ -227,43 +223,42 @@ class CoursePage extends Component{
 		let path = "Users/" + this.context.user.uid + "/progress/" + skillName
     	let myRef = root.child(path)
 
-    	//創建進度
-		let progress = 1 / childrenLength * skillStdName.length
-		if(0.95 <= progress && progress <= 1){
-			progress = 1
-			//刪除progress資料，更新學習歷程
-
-		}else{
-			//更新progress
-		}
-		if(progress <= 0.05){
-			progress = 0
-		}
-
-		//創建std紀錄
-		let childrenData = new Object()
-		skillStdName.forEach((stdName, index) => {
-			childrenData[index] = stdName
-		})
-
-		console.log("SS:", "updateProgress")
     	myRef.once('value', (snapshot)=> {
 			let data = snapshot.val()
 			if(data){
-				
+				let progress = data['completed']
+				let completedNum = data['children'].length
+				//如果一開始沒有,data預設為0
+				if(!completedNum){
+					completedNum = 0
+				}
+
+				//update skill standard information
+				let childrenData = new Object()
+				childrenData[completedNum] = skillStdName
+				myRef.child("children").update(childrenData)
+
+
+				//update progress
+				progress = progress + 1/childrenLength
+
+				//避免因除法導致小數點未進位
+				if(0.95 <= progress && progress <= 1){
+					progress = 1
+				}
 				myRef.update({
-					"children": childrenData,
 					"completed": progress
 				})
-
 			}else{
 				let path = "Users/" + this.context.user.uid + "/progress"
 				let myRef = root.child(path)
 
 				let progressData = new Object()
 				progressData[skillName] = {
-					"children": childrenData,
-					"completed": progress
+					"children":{
+						0: skillStdName
+					},
+					"completed": 1/childrenLength
 				}
 				myRef.update(progressData)
 			}
@@ -273,26 +268,7 @@ class CoursePage extends Component{
     	})
 	}
 
-	updateTreeStateProgress(path, stdLength, childrenLength){
-
-    	//創建進度
-		let progress = 1 / childrenLength * stdLength
-		if(0.95 <= progress && progress <= 1){
-			progress = 1
-		}
-		if(progress <= 0.05){
-			progress = 0
-		}
-
-		let myRef = root.child(path)
-		myRef.update({
-			"completed": progress
-		})	
-
-	}
-
 	render() {
-
 		return (
 			<div className='container' style={{'paddingTop':'10vh'}}>
 				<div className='CoursePage row row-style'>
